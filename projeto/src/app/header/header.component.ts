8d10ee12e7e2c9251a2659f3cffddb3d4ff3e918
josuejorge <josue.jorge.oliveira@accenture.com>
2019-09-03 16:50:45 -0300

    tela de login

--- projeto/src/app/header/header.component.ts
+++ projeto/src/app/header/header.component.ts
@@ -1,12 +1,17 @@
+// aqui estamos importando o component do pacote angular core
 import { Component, OnInit } from '@angular/core';
 
+//  Aqui decoramos a classe do component App Header
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
+// Aqui exportamos a classe HeaderComponent
 export class HeaderComponent implements OnInit {
 
+    // aqui definimos o constructor da aplicacao
   constructor() { }
 
   ngOnInit() {
